a5626e33bbd52c6c233e8731c001b18e70b040e7
xescugc <xescugil@gmail.com>
2026-05-15 22:07:16 +0200

    Update CLI usage description to self-hosted, portable

--- cmd/root.go
+++ cmd/root.go
@@ -11,7 +11,7 @@ var (
 var (
 	Cmd = &cli.Command{
 		Name:  AppName,
-		Usage: "PikoCI is a small CI/CD build on top of a Queue(Pub/Sub) system",
+		Usage: "PikoCI is a self-hosted, portable CI/CD system",
 		Commands: []*cli.Command{
 			serverCmd,
 			clientCmd,
