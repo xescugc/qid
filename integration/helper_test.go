17d768f7628f34ff1e1f69255590ac64aa32846f
xescugc <xescugil@gmail.com>
2026-05-17 00:56:31 +0200

    Fix integration tests: use geckodriver directly with portable paths
    - Remove Selenium Server dependency (incompatible with geckodriver 0.36)
    - Drive geckodriver directly instead of through Selenium Server
    - Use runtime.Caller to resolve geckodriver path relative to source file
    - Use 127.0.0.1 instead of localhost to avoid IPv6 resolution issues

--- integration/helper_test.go
+++ integration/helper_test.go
@@ -2,35 +2,64 @@ package integration_test
 
 import (
 	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"github.com/tebeka/selenium"
 )
 
 const (
-	// These paths will be different on your system.
-	seleniumPath    = "/home/xescugc/repos/qid/integration/vendor/selenium-server.jar"
-	geckoDriverPath = "/home/xescugc/repos/qid/integration/vendor/geckodriver"
-	port            = 8080
+	port = 8080
 )
 
+var geckoDriverPath string
+
+func init() {
+	_, f, _, _ := runtime.Caller(0)
+	geckoDriverPath = filepath.Join(filepath.Dir(f), "vendor", "geckodriver")
+}
+
 func getRemote(t *testing.T) selenium.WebDriver {
-	opts := []selenium.ServiceOption{
-		selenium.StartFrameBuffer(),           // Start an X frame buffer for the browser to run in.
-		selenium.GeckoDriver(geckoDriverPath), // Specify the path to GeckoDriver in order to use Firefox.
-		//selenium.Output(os.Stderr),            // Output debug information to STDERR.
-	}
-	//selenium.SetDebug(true)
-	service, err := selenium.NewSeleniumService(seleniumPath, port, opts...)
+	// Start Xvfb for headless browser testing.
+	fbuf, err := selenium.NewFrameBuffer()
 	require.NoError(t, err)
 	t.Cleanup(func() {
-		service.Stop()
+		fbuf.Stop()
 	})
+
+	// Start geckodriver bound to 127.0.0.1 (the default).
+	cmd := exec.Command(geckoDriverPath, "--port", strconv.Itoa(port))
+	cmd.Env = append(os.Environ(), "DISPLAY=:"+fbuf.Display, "XAUTHORITY="+fbuf.AuthPath)
+	err = cmd.Start()
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		cmd.Process.Kill()
+		cmd.Wait()
+	})
+
+	// Wait for geckodriver to be ready.
+	addr := fmt.Sprintf("http://127.0.0.1:%d", port)
+	for i := 0; i < 30; i++ {
+		time.Sleep(time.Second)
+		resp, err := http.Get(addr + "/status")
+		if err == nil {
+			resp.Body.Close()
+			if resp.StatusCode == http.StatusOK {
+				break
+			}
+		}
+	}
 
 	// Connect to the WebDriver instance running locally.
 	caps := selenium.Capabilities{"browserName": "firefox"}
-	wd, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", port))
+	wd, err := selenium.NewRemote(caps, addr)
 	require.NoError(t, err)
 	t.Cleanup(func() {
 		wd.Quit()
